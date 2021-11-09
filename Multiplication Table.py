e5714f348d0523b44f468d88d436524a34019c57
Jishnu M M <91713896+ThePyWizard@users.noreply.github.com>
2021-11-09 14:54:33 +0530

    Create Multiplication Table.py

---
+++ Multiplication Table.py
@@ -0,0 +1,8 @@
+# For multiplication table
+def probius_multi():
+    print("For multiplication table")
+    b=int(input("Enter a number for the multiplication table : "))
+    n=int(input("Enter a limit 10 or more than 10 as you like : "))
+    for i in range(1,n+1):
+        j=i*b
+        print(i,'x',b,'=',j)
